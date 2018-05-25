62c80272b1d9607a6cc179b23f907122742de750
Sir Lucky <howlzu@protonmail.com>
2018-05-24 18:36:35 -0700

    TxID search

--- src/components/ArtifactViewer.js
+++ src/components/ArtifactViewer.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react'
 class ArtifactViewer extends Component {
 	render(){
 		return(
-			<div>Artifact Viewer</div>
+		<div className="card" style={{marginTop:"20px"}}>
+  			<div className="card-body">
+    					<p>Part Number: <code>$partNumber</code></p>
+						<p>Total Multipart Length: <code>$multipartArrayLength</code></p>
+					<div className="alert alert-secondary" role="alert">
+							OIP-MP($partNumber, $multipartArrayLength, $PublisherAddress, $firstPartTXID, $Signature):choppedStringData
+  					</div>
+			</div>
+		</div>
 		)
 	}
 }
--- src/components/Lookup.js
+++ src/components/Lookup.js
@@ -6,12 +6,23 @@ import MultipartViewer from './MultipartViewer.js'
 class Lookup extends Component {
 	render(){
 		return(
-			<div>
+		<div className="container">
+			<h1>Artifact Lookup</h1>
+			<div className="input-group mb-3">
+  			<input type="text" className="form-control" placeholder="Txid" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+  			<div className="input-group-append">
+    			<button className="btn btn-outline-secondary" type="button">Lookup</button>
+  			</div>
+			  </div>
+		
+				
 				<MultipartViewer />
 				<MultipartViewer />
 				<MultipartViewer />
 				<ArtifactViewer />
-			</div>
+			
+			
+		</div>
 		)
 	}
 }
