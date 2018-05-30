2cbaa4dd2ebc3f6a7e8013efeeaf8ae830567333
Howl0s <howlzu@protonmail.com>
2018-05-30 13:24:00 -0700

    navbar added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Artifact from './Artifact';
+import ArtifactViewer from './ArtifactViewer';
 
 class Header extends Component {
 	render(){
 		return(
-			<div>Header</div>
+		<div className="Head"> 
+			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    				<span className="navbar-toggler-icon"></span>
+  			</button>
+  			<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+    		<div className="navbar-nav">
+      				<a className="nav-item nav-link active" href="/">OIP Artifact Explorer<span className="sr-only">(current)</span></a>
+      				<a className="nav-item nav-link" href="/multipart">Multipart Inspector</a>
+      				<a className="nav-item nav-link" href="/artifact">Artifact Inspector</a>
+    		</div>
+  			</div>
+			</nav>
+		</div>
 		)
 	}
 }
