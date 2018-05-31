483d0f3664bc1866f6ad8a0b119922344496c641
Howl0s <howlzu@protonmail.com>
2018-05-31 13:39:31 -0700

    Images added to downloader + removal of stock images with adding of FontAwesome

--- src/components/DownloadFileList.js
+++ src/components/DownloadFileList.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-import File from '../assets/imgs/File.svg';
-import Folder from '../assets/imgs/Folder.svg';
-import Image from '../assets/imgs/Image.svg';
-import Video from '../assets/imgs/Video.svg';
+import File from '../assets/imgs/file.svg';
+import Zip from '../assets/imgs/file-archive.svg';
+import Image from '../assets/imgs/file-image.svg';
+import Video from '../assets/imgs/file-video.svg';
+import Pdf from '../assets/imgs/file-pdf.svg';
+import Code from '../assets/imgs/file-code.svg';
+import Audio from '../assets/imgs/file-code.svg';
+import Text from '../assets/imgs/file-alt.svg';
 
 
 class DownloadFileList extends Component {
@@ -13,33 +17,46 @@ class DownloadFileList extends Component {
         console.log(this.state)
     return(
     <div className="container">
-       <table class="table">
+       <table className="table table-hover">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col"></th>
+      <th scope="col" style= {{maxWidth:"10px"}}></th>
+      <th scope="col">Filename</th>
+      <th scope="col">Type</th>
+      <th scope="col">Size</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <th scope="row"><div className="form-check">
+        <input className="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="..."/>
+        </div>
+        </th>
+        <td><img width="30" height="30" src={Video}/></td>
+      <td>keymov_rr2014-10-17-24.mp4</td>
+      <td>Key Movie</td>
+      <td>11.68 MB</td>
     </tr>
     <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
+      <th scope="row"><div className="form-check">
+        <input className="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="..."/>
+        </div>
+        </th>
+        <td><img width="30" height="30" src={File}/></td>
+      <td>nmar20024_part121_17.rec</td>
+      <td>Reconstruction</td>
+      <td>445.44 MB</td>
     </tr>
     <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
+      <th scope="row"><div className="form-check">
+        <input className="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="..."/>
+        </div>
+        </th>
+        <td><img width="30" height="30" src={File}/></td>
+      <td>nmar20024.mrc</td>
+      <td>Tilt series</td>
+      <td>3.45 GB </td>
     </tr>
   </tbody>
 </table>
