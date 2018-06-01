a3070cc772fc56027510ee6d55552fb031062aab
Howl0s <howlzu@protonmail.com>
2018-05-31 18:02:33 -0700

    Code for rendering of Downloader

---
+++ src/components/BdcWrapper.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+import BulkDownloadContainer from './BulkDownloadContainer.js'
+
+class BdcWrapper extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            artID: "51ebca",
+            artifact: undefined
+        }
+
+        this.updateArtifactText = this.updateArtifactText.bind(this);
+        this.getArtifactFromID = this.getArtifactFromID.bind(this);
+    }
+    updateArtifactText(event){
+        console.log(event.target.value)
+        this.setState({
+            artID: event.target.value
+        })
+    }
+    componentDidMount(){
+        this.getArtifactFromID("51ebca");
+    }
+    getArtifactFromID(artID){
+        var self = this;
+
+        this.props.Core.Index.getArtifactFromID(artID, function(art){
+            self.setState({
+                artifact: art
+            })
+        }, function(err){
+            console.error(err)
+        })
+    }
+    render() 
+    { 
+        return (
+            <div>
+                <BulkDownloadContainer artifact={this.state.artifact}/>
+            </div>
+        )
+    }
+}
+
+ 
+export default BdcWrapper;
