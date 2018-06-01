d88b71a9baec65f1cf524294f9e232123e3dc300
Howl0s <howlzu@protonmail.com>
2018-05-31 18:04:49 -0700

    Downloader added within Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,9 +20,10 @@ class Header extends Component {
   			</button>
   			<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
     		<div className="navbar-nav">
-      				<a className="nav-item nav-link active" href="/">Artifact Explorer<span className="sr-only">(current)</span></a>
+      				<a className="nav-item nav-link" href="/">Artifact Explorer<span className="sr-only">(current)</span></a>
       				<a className="nav-item nav-link" href="/multipart">Multipart Inspector</a>
       				<a className="nav-item nav-link" href="/artifact">Artifact Inspector</a>
+					<a className="nav-item nav-link" href="/BulkDownloadContainer">Downloader</a>
     		</div>
   			</div>
 			</nav>
