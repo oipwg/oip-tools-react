43b9c8c1915d9d90797dc5b42abf480f1630e82a
Anthony Howl <anthonyhowl@protonmail.ch>
2018-06-13 12:58:15 -0700

    Removed Downloader

--- src/components/AppRoutes.js
+++ src/components/AppRoutes.js
@@ -9,9 +9,8 @@ import Lookup from './Lookup.js';
 import Artifact from './Artifact.js';
 import Multipart from './Multipart.js';
 import Header from './Header';
-import DownloadFileList from './DownloadFileList.js';
-import BdcWrapper from './BdcWrapper.js';
-import Loader from './Loader.js';
+
+
 
 class AppRoutes extends Component {
 	render(){
@@ -23,8 +22,6 @@ class AppRoutes extends Component {
 					<Route path="/artifact" render={props => <Artifact Core={this.props.Core} {...props} />} />
 					<Route path="/multipart" render={props => <Multipart Core={this.props.Core} {...props} />} />
 					<Route path="/DownloadFileList" render={props => <DownloadFileList Core={this.props.Core}{...props} />} />
-					<Route path="/Bulk" render={props => <BdcWrapper Core={this.props.Core}{...props} />} />
-					<Route path="/load" render={props => <Loader Core={this.props.Core}{...props} />} />
 					<Route render={props => <Lookup Core={this.props.Core} {...props} />} />
 					
 				</Switch>
